5e702476c068a97d4dbfc5631fd628f688e1bda9
Tako_viao <takotabak@gmail.com>
2018-12-20 10:19:04 +0100

    without docker overhead was too big

--- start.sh
+++
@@ -1,6 +0,0 @@
-DIR=SVHN
-LABEL=svhn
-sudo docker kill $(sudo docker ps -q)
-sudo docker build -t $LABEL .
-ECHO $LABEL
-sudo docker run -it -p 8888:8888 -v /home/tako/testroom/$DIR/dockervolume/:/home/jpuser/dockerhostvolume  $LABEL
